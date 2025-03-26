975a9b7c24110e383436da1342f10ce62ec48614
Xhemal Kodragjini <47697000+xhemalkodragjini@users.noreply.github.com>
2025-03-26 11:14:59 +0100

    Update views.py

--- Test/views.py
+++ Test/views.py
@@ -12,7 +12,7 @@ def test_view(request):
     form = TestForm(request.POST or None)
     if form.is_valid():
         form.save()
-        loaded_model = pickle.load(open('C:/Users/Kodragjini/Documents/Poki/Test/m.sav', 'rb'))
+        loaded_model = pickle.load(open('m.sav', 'rb'))
         qs = TestModel.objects.all().values('a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7',
                                             'a8', 'a9', 'a10', 'mosha_ne_muaj','gjinia',
                                             'etnia', 'verdheza',
